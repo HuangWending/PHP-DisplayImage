2bb86c69b0f27395ef5f836a6543fead1405bc8a
黃(黄)文定-ㄏㄨㄤˊ ㄨㄣˊㄉㄧㄥˋ <91007091+HuangWending@users.noreply.github.com>
2023-06-06 20:52:18 +0800

    Add files via upload

---
+++ DisplayImage.php
@@ -0,0 +1,12 @@
+<?php
+    $imagePath = 'path_to_your_image';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>显示路径图片</title>
+</head>
+<body>
+    <img src="<?php echo $imagePath; ?>" alt="图片">
+</body>
+</html>
